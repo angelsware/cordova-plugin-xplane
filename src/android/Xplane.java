c4b19db4d49a15928f1742ea6311363244e11c6b
Joachim Klahr <joachim@angelsware.com>
2017-08-18 14:09:54 +0200

    Cleanup.

--- src/android/Xplane.java
+++ src/android/Xplane.java
@@ -10,8 +10,6 @@ import org.json.JSONObject;
 import gov.nasa.xpc.XPlaneConnect;
 
 public class Xplane extends CordovaPlugin {
-	private XPlaneConnect mXpc = null;
-
 	public Xplane() {
 	}
 
@@ -40,164 +38,135 @@ public class Xplane extends CordovaPlugin {
 		return true;
 	}
 
-	private void connect(JSONArray args, CallbackContext callbackContext) {
+	private void connect(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("method", "connect");
 		try {
-			JSONObject json = new JSONObject();
-			try {
-				mXpc = new XPlaneConnect(args.getString(0), args.getInt(1), args.getInt(2));
-				json.put("result","ok");
-				json.put("method", "connect");
-			} catch (Exception ex) {
-				json.put("result","error");
-				json.put("method", "connect");
-				json.put("message", ex.getMessage());
-			}
-			callbackContext.success(json);
+			mXpc = new XPlaneConnect(args.getString(0), args.getInt(1), args.getInt(2));
+			json.put("result","ok");
 		} catch (Exception ex) {
+			json.put("result","error");
+			json.put("message", ex.getMessage());
 		}
+		callbackContext.success(json);
 	}
 
-	private void getDREF(JSONArray args, CallbackContext callbackContext) {
+	private void getDREF(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		String dref = args.getString(0);
+		JSONObject r = null;
 		try {
-			String dref = args.getString(0);
-			JSONObject r = null;
-			try {
-				r = successDREFResult(dref, "getDREF", mXpc.getDREF(dref));
-			} catch (Exception ex) {
-				r = failedDREFResult(dref, "getDREF", ex.getMessage());
-			}
-			callbackContext.success(r);
+			r = successDREFResult(dref, "getDREF", mXpc.getDREF(dref));
 		} catch (Exception ex) {
+			r = failedDREFResult(dref, "getDREF", ex.getMessage());
 		}
+		callbackContext.success(r);
 	}
 
-	private void sendDREF(JSONArray args, CallbackContext callbackContext) {
+	private void sendDREF(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		String dref = args.getString(0);
+		JSONObject r = new JSONObject();
 		try {
-			String dref = args.getString(0);
-			JSONObject r = new JSONObject();
-			try {
-				float[] values = new float[args.getJSONArray(1).length()];
-				for (int i = 0; i < args.getJSONArray(1).length(); ++i) {
-					values[i] = (float)args.getJSONArray(1).getDouble(i);
-				}
-				mXpc.sendDREF(dref, values);
-				r = successDREFResult(dref, "sendDREF", null);
-			} catch (Exception ex) {
-				r = failedDREFResult(dref, "sendDREF", ex.getMessage());
+			float[] values = new float[args.getJSONArray(1).length()];
+			for (int i = 0; i < args.getJSONArray(1).length(); ++i) {
+				values[i] = (float)args.getJSONArray(1).getDouble(i);
 			}
-			callbackContext.success(r);
+			mXpc.sendDREF(dref, values);
+			r = successDREFResult(dref, "sendDREF", null);
 		} catch (Exception ex) {
+			r = failedDREFResult(dref, "sendDREF", ex.getMessage());
 		}
+		callbackContext.success(r);
 	}
 
-	private void sendPOSI(JSONArray args, CallbackContext callbackContext) {
+	private void sendPOSI(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("method", "sendPOSI");
+		float[] values = new float[args.getJSONArray(0).length()];
+		for (int i = 0; i < args.getJSONArray(0).length(); ++i) {
+			values[i] = (float)args.getJSONArray(0).getDouble(i);
+		}
 		try {
-			float[] values = new float[args.getJSONArray(0).length()];
-			for (int i = 0; i < args.getJSONArray(0).length(); ++i) {
-				values[i] = (float)args.getJSONArray(0).getDouble(i);
-			}
 			mXpc.sendPOSI(values, args.getInt(1));
-			JSONObject json = new JSONObject();
 			json.put("result", "ok");
-			json.put("method", "sendPOSI");
-			callbackContext.success(json);
 		} catch (Exception ex) {
-			JSONObject json = new JSONObject();
-			try {
-				json.put("result", "error");
-				json.put("method", "sendPOSI");
-				json.put("message", ex.getMessage());
-			} catch (Exception e) {
-			}
-			callbackContext.success(json);
-		}
-	}
-
-	private void getPOSI(JSONArray args, CallbackContext callbackContext) {
-		JSONObject json = new JSONObject();
-		try {
-			try {
-				json.put("method", "getPOSI");
-				float[] values = mXpc.getPOSI(args.getInt(0));
-				json.put("result", "ok");
-				JSONArray a = new JSONArray();
-				for (int i = 0; i < values.length; ++i) {
-					a.put(values[i]);
-				}
-				json.put("values", a);
-			} catch (Exception ex) {
-				json.put("result", "error");
-				json.put("message", ex.getMessage());
-			}
-		} catch (Exception ex) {
-		}
-		callbackContext.success(json);
-	}
-
-	private void sendTEXT(JSONArray args, CallbackContext callbackContext) {
-		JSONObject json = new JSONObject();
-		try {
-			json.put("method", "sendTEXT");
-			try {
-				mXpc.sendTEXT(args.getString(0));
-				json.put("result", "ok");
-			} catch (Exception ex) {
-				json.put("result", "error");
-				json.put("message", ex.getMessage());
-			}
-		} catch (Exception e) {
-		}
-		callbackContext.success(json);
-	}
-
-	private void getCTRL(JSONArray args, CallbackContext callbackContext) {
-		JSONObject json = new JSONObject();
-		try {
-			try {
-				json.put("method", "getCTRL");
-				float[] values = mXpc.getCTRL(args.getInt(0));
-				json.put("result", "ok");
-				JSONArray a = new JSONArray();
-				for (int i = 0; i < values.length; ++i) {
-					a.put(values[i]);
-				}
-				json.put("values", a);
-			} catch (Exception ex) {
-				json.put("result", "error");
-				json.put("message", ex.getMessage());
-			}
-		} catch (Exception ex) {
-		}
-		callbackContext.success(json);
-	}
-
-	private JSONObject successDREFResult(String dref, String method, float[] values) {
-		JSONObject json = new JSONObject();
-		try {
-			json.put("result", "ok");
-			json.put("method", method);
-			json.put("dref", dref);
-			if (values != null) {
-				JSONArray a = new JSONArray();
-				for (int i = 0; i < values.length; ++i) {
-					a.put(values[i]);
-				}
-				json.put("values", a);
-			}
-		} catch (Exception ex) {
-		}
-		return json;
-	}
-
-	private JSONObject failedDREFResult(String dref, String method, String message) {
-		JSONObject json = new JSONObject();
-		try {
 			json.put("result", "error");
-			json.put("method", method);
-			json.put("dref", dref);
-			json.put("message", message);
+			json.put("message", ex.getMessage());
+		}
+		callbackContext.success(json);
+	}
+
+	private void getPOSI(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("method", "getPOSI");
+		try {
+			float[] values = mXpc.getPOSI(args.getInt(0));
+			json.put("result", "ok");
+			JSONArray a = new JSONArray();
+			for (int i = 0; i < values.length; ++i) {
+				a.put(values[i]);
+			}
+			json.put("values", a);
 		} catch (Exception ex) {
+			json.put("result", "error");
+			json.put("message", ex.getMessage());
+		}
+		callbackContext.success(json);
+	}
+
+	private void sendTEXT(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("method", "sendTEXT");
+		try {
+			mXpc.sendTEXT(args.getString(0));
+			json.put("result", "ok");
+		} catch (Exception ex) {
+			json.put("result", "error");
+			json.put("message", ex.getMessage());
+		}
+		callbackContext.success(json);
+	}
+
+	private void getCTRL(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("method", "getCTRL");
+		try {
+			float[] values = mXpc.getCTRL(args.getInt(0));
+			json.put("result", "ok");
+			JSONArray a = new JSONArray();
+			for (int i = 0; i < values.length; ++i) {
+				a.put(values[i]);
+			}
+			json.put("values", a);
+		} catch (Exception ex) {
+			json.put("result", "error");
+			json.put("message", ex.getMessage());
+		}
+		callbackContext.success(json);
+	}
+
+	private JSONObject successDREFResult(String dref, String method, float[] values) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("result", "ok");
+		json.put("method", method);
+		json.put("dref", dref);
+		if (values != null) {
+			JSONArray a = new JSONArray();
+			for (int i = 0; i < values.length; ++i) {
+				a.put(values[i]);
+			}
+			json.put("values", a);
 		}
 		return json;
 	}
+
+	private JSONObject failedDREFResult(String dref, String method, String message) throws JSONException {
+		JSONObject json = new JSONObject();
+		json.put("result", "error");
+		json.put("method", method);
+		json.put("dref", dref);
+		json.put("message", message);
+		return json;
+	}
+
+	private XPlaneConnect mXpc = null;
 }
